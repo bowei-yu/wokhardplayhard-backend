9ff4ae1e4f5499aeda03766e3443dd83c65a7c11
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-06-03 18:59:22 +0800

    adding files

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    recipes: [
+        {
+            userHandle: 'user',
+            body: 'this is the recipe body',
+            createdAt: '2011-10-05T14:48:00.000Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
